1b71e4d6a9cc683a8d7bcc75cab949c801e148b1
Kelly Roach <kroach@crittercism.com>
2015-04-28 14:24:43 -0700

    Format *.cs

--- Editor/Crittercism/CrittercismEditor.cs
+++ Editor/Crittercism/CrittercismEditor.cs
@@ -2,51 +2,52 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
+////////////////////////////////////////////////////////////////
+// NOTE: CrittercismEditor is for older versions of Unity prior
+// to Unity 5.0 .  For Unity 5.0 or later, it is not necessary
+// to add an AppController.mm file to your app sources.
+// Consult http://docs.crittercism.com/development_platforms/integration_unity_ios.html
+// "Installing the iOS Unity SDK" for Unity 5.0 or later.
+////////////////////////////////////////////////////////////////
+
 public class CrittercismEditor : EditorWindow
 {
-	string mApplicationID	= null;
-	
+	string mApplicationID = null;
 	
 	[MenuItem ("Crittercism/Setup IOS")]
-    public static void Setup ()
+	public static void Setup ()
 	{
-      	CrittercismEditor window = (CrittercismEditor)EditorWindow.GetWindow(typeof(CrittercismEditor));
-		window.mApplicationID	= EditorPrefs.GetString("CrittercismAppID", "");
-      	window.Show();
+		CrittercismEditor window = (CrittercismEditor)EditorWindow.GetWindow (typeof(CrittercismEditor));
+		window.mApplicationID = EditorPrefs.GetString ("CrittercismAppID", "");
+		window.Show ();
 	}
 	
 	void OnGUI ()
 	{
 		GUILayout.Label ("Crittercism", EditorStyles.boldLabel);
 		mApplicationID = EditorGUILayout.TextField ("Application ID", mApplicationID);
-		
-		GUILayoutOption[] options	= new GUILayoutOption[0];
-		if(GUILayout.Button("Setup",options))
-		{
-			if(string.IsNullOrEmpty(mApplicationID))
-			{
-				UnityEditor.EditorUtility.DisplayDialog("Error", "No Crittercism Application ID Entered", "Close");
+		GUILayoutOption[] options = new GUILayoutOption[0];
+		if (GUILayout.Button ("Setup", options)) {
+			if (string.IsNullOrEmpty (mApplicationID)) {
+				UnityEditor.EditorUtility.DisplayDialog ("Error", "No Crittercism Application ID Entered", "Close");
 				return;
 			}
-			
-			EditorPrefs.SetString("CrittercismAppID", mApplicationID);
-			string ret	= HandleDynamicCrittercismSetup(mApplicationID);
-			if(!string.IsNullOrEmpty(ret))
-			{
-				string retHardcode	= HandleHardcodedCrittercismSetup (mApplicationID);
-				if(!string.IsNullOrEmpty(retHardcode))
-				{
-					UnityEditor.EditorUtility.DisplayDialog("Error", ret, "Close");
+			EditorPrefs.SetString ("CrittercismAppID", mApplicationID);
+			string ret = HandleDynamicCrittercismSetup (mApplicationID);
+			if (!string.IsNullOrEmpty (ret)) {
+				string retHardcode = HandleHardcodedCrittercismSetup (mApplicationID);
+				if (!string.IsNullOrEmpty (retHardcode)) {
+					UnityEditor.EditorUtility.DisplayDialog ("Error", ret, "Close");
 					return;
 				}
 			}
-			UnityEditor.EditorUtility.DisplayDialog("Success", "Crittercisms AppController Was Installed Successfully.", "Close");
+			UnityEditor.EditorUtility.DisplayDialog ("Success", "Crittercisms AppController Was Installed Successfully.", "Close");
 		}
 	}
 	
-	public static string HandleHardcodedCrittercismSetup(string applicationID)
+	public static string HandleHardcodedCrittercismSetup (string applicationID)
 	{
-		string acFile	= "AppController";
+		string acFile = "AppController";
 #if UNITY_3_4
 		acFile	+= "3.4.0";
 #elif UNITY_3_5 && !UNITY_3_5_7
@@ -60,159 +61,126 @@ public class CrittercismEditor : EditorWindow
 #elif UNITY_4_5
 		acFile	+= "4.5";
 #else
-		UnityEditor.EditorUtility.DisplayDialog("Error", "Unknown Version of Unity!", "Close");
+		UnityEditor.EditorUtility.DisplayDialog ("Error", "Unknown Version of Unity!", "Close");
 		return;
 #endif
-		
-		string writePath	= GetIOSPluginsFolder() + "/AppController.mm";
+		string writePath = GetIOSPluginsFolder () + "/AppController.mm";
 		acFile = Application.dataPath + "/Editor/Crittercism/" + acFile + ".mm";
-		try
-		{
-			if(!System.IO.File.Exists(acFile))
-			{	throw new System.Exception("Invalid File Path For AppController:\n" + acFile);	}
-			
-			if(System.IO.File.Exists(writePath))
-			{
-				try
-				{
-					System.IO.File.Delete(writePath);
-				}catch
-				{	throw new System.Exception("Failed to Delete File For AppController:\n" + writePath);	}
+		try {
+			if (!System.IO.File.Exists (acFile)) {
+				throw new System.Exception ("Invalid File Path For AppController:\n" + acFile);
 			}
-		}catch(System.Exception e)
-		{	return e.Message;	}
-		
-		try
-		{
-			System.IO.StreamReader readFile		= new System.IO.StreamReader(acFile);
-			System.IO.StreamWriter writeFile	= new System.IO.StreamWriter(writePath);
-			
-			bool applicationIDWasWritten	= false;
-			string lineData	= null;
-			while((lineData = readFile.ReadLine()) != null)
-			{
-				if(!applicationIDWasWritten)
-				{
-					if(lineData.Contains("\"CRITTERCISM_APP_ID\""))
-					{
-						lineData	= lineData.Replace("CRITTERCISM_APP_ID", applicationID);
-						applicationIDWasWritten	= true;
+			if (System.IO.File.Exists (writePath)) {
+				try {
+					System.IO.File.Delete (writePath);
+				} catch {
+					throw new System.Exception ("Failed to Delete File For AppController:\n" + writePath);
+				}
+			}
+		} catch (System.Exception e) {
+			return e.Message;
+		}
+		try {
+			System.IO.StreamReader readFile = new System.IO.StreamReader (acFile);
+			System.IO.StreamWriter writeFile = new System.IO.StreamWriter (writePath);
+			bool applicationIDWasWritten = false;
+			string lineData = null;
+			while ((lineData = readFile.ReadLine()) != null) {
+				if (!applicationIDWasWritten) {
+					if (lineData.Contains ("\"CRITTERCISM_APP_ID\"")) {
+						lineData = lineData.Replace ("CRITTERCISM_APP_ID", applicationID);
+						applicationIDWasWritten = true;
 					}
 				}
-				
-				writeFile.Write(lineData + "\n");
+				writeFile.Write (lineData + "\n");
 			}
-			
-			readFile.Close();
-			readFile	= null;
-			writeFile.Flush();
-			writeFile.Close();
-			writeFile	= null;
-			
-		}catch
-		{	return "Unable to finish writting file data.";	}
-		
+			readFile.Close ();
+			readFile = null;
+			writeFile.Flush ();
+			writeFile.Close ();
+			writeFile = null;
+		} catch {
+			return "Unable to finish writting file data.";
+		}
 		return null;
 	}
 	
-	public static string HandleDynamicCrittercismSetup(string applicationID)
+	public static string HandleDynamicCrittercismSetup (string applicationID)
 	{
-		string iphoneDataPath		= UnityEditor.EditorApplication.applicationContentsPath + "/PlaybackEngines/iPhonePlayer/iPhone-Trampoline/";
-		string appControllerPath	= null;
-		try
-		{
-			string[] files	= System.IO.Directory.GetFiles(iphoneDataPath, "*.mm", System.IO.SearchOption.AllDirectories);
-			for(int nItter = 0; nItter < files.Length; nItter++)
-			{
-				if(files[nItter].Contains("AppController.mm"))
-				{
-					appControllerPath	= files[nItter];
+		string iphoneDataPath = UnityEditor.EditorApplication.applicationContentsPath + "/PlaybackEngines/iPhonePlayer/iPhone-Trampoline/";
+		string appControllerPath = null;
+		try {
+			string[] files = System.IO.Directory.GetFiles (iphoneDataPath, "*.mm", System.IO.SearchOption.AllDirectories);
+			for (int nItter = 0; nItter < files.Length; nItter++) {
+				if (files [nItter].Contains ("AppController.mm")) {
+					appControllerPath = files [nItter];
 					break;
 				}
 			}
+		} catch (System.Exception e) {
+			return e.Message;
 		}
-		catch(System.Exception e)
-		{	return e.Message;	}
-		
-		if(string.IsNullOrEmpty(appControllerPath))
-		{	return "Failed To Locate IOS App Controller";	}
-		
-		try
-		{
-			string writePath	= GetIOSPluginsFolder() + "/AppController.mm";
-			System.IO.StreamReader readFile		= new System.IO.StreamReader(appControllerPath);
-			System.IO.StreamWriter writeFile	= new System.IO.StreamWriter(writePath);
-			
-			bool applicationIDWasWritten	= false;
-			bool hasWritenExtern			= false;
-			string lineData	= null;
-			while((lineData = readFile.ReadLine()) != null)
-			{
-				if(!applicationIDWasWritten)
-				{
-					if(!hasWritenExtern)
-					{
-						if(lineData.Contains("//"))
-						{
-							writeFile.Write("const char* kCrittercism_App	= \"" + applicationID + "\";\n");
-							writeFile.Write("extern \"C\" void Crittercism_EnableWithAppID(const char* appID);\n\n");
-							hasWritenExtern	= true;
+		if (string.IsNullOrEmpty (appControllerPath)) {
+			return "Failed To Locate IOS App Controller";
+		}
+		try {
+			string writePath = GetIOSPluginsFolder () + "/AppController.mm";
+			System.IO.StreamReader readFile = new System.IO.StreamReader (appControllerPath);
+			System.IO.StreamWriter writeFile = new System.IO.StreamWriter (writePath);
+			bool applicationIDWasWritten = false;
+			bool hasWritenExtern = false;
+			string lineData = null;
+			while ((lineData = readFile.ReadLine()) != null) {
+				if (!applicationIDWasWritten) {
+					if (!hasWritenExtern) {
+						if (lineData.Contains ("//")) {
+							writeFile.Write ("const char* kCrittercism_App	= \"" + applicationID + "\";\n");
+							writeFile.Write ("extern \"C\" void Crittercism_EnableWithAppID(const char* appID);\n\n");
+							hasWritenExtern = true;
 						}
-					}
-					else
-					{
-						if(lineData.Contains("-"))
-						{
-							if(lineData.Contains("didFinishLaunchingWithOptions:"))
-							{
-								writeFile.Write(lineData + "\n");
-								lineData = readFile.ReadLine();
-								if(lineData.Contains("{") && !lineData.Contains("if"))
-								{
-									writeFile.Write(lineData + "\n");
-									lineData = readFile.ReadLine();
+					} else {
+						if (lineData.Contains ("-")) {
+							if (lineData.Contains ("didFinishLaunchingWithOptions:")) {
+								writeFile.Write (lineData + "\n");
+								lineData = readFile.ReadLine ();
+								if (lineData.Contains ("{") && !lineData.Contains ("if")) {
+									writeFile.Write (lineData + "\n");
+									lineData = readFile.ReadLine ();
 								}
-								writeFile.Write("	Crittercism_EnableWithAppID(kCrittercism_App);\n\n");
-								applicationIDWasWritten	= true;
+								writeFile.Write ("	Crittercism_EnableWithAppID(kCrittercism_App);\n\n");
+								applicationIDWasWritten = true;
 							}
 						}
 					}
 				}
-				
-				writeFile.Write(lineData + "\n");
+				writeFile.Write (lineData + "\n");
 			}
-			
-			readFile.Close();
-			readFile	= null;
-			writeFile.Flush();
-			writeFile.Close();
-			writeFile	= null;
-			
-		}catch
-		{	return "Unable to finish writting file data.";	}
-		
+			readFile.Close ();
+			readFile = null;
+			writeFile.Flush ();
+			writeFile.Close ();
+			writeFile = null;
+		} catch {
+			return "Unable to finish writting file data.";
+		}
 		return null;
 	}
 	
-	private static string GetIOSPluginsFolder()
+	private static string GetIOSPluginsFolder ()
 	{
-		string fileDirectory	= "ios";
-		try
-		{
-			string[] dirs	= System.IO.Directory.GetDirectories(Application.dataPath + "/Plugins/");
-			for(int nItter = 0; nItter < dirs.Length; nItter++)
-			{
+		string fileDirectory = "ios";
+		try {
+			string[] dirs = System.IO.Directory.GetDirectories (Application.dataPath + "/Plugins/");
+			for (int nItter = 0; nItter < dirs.Length; nItter++) {
 				//	HACK: Support the bugged versions
-				string temp	= dirs[nItter].ToLower() + "/";
-				if(temp.Contains("plugins/ios/"))
-				{
-					fileDirectory	= dirs[nItter];
+				string temp = dirs [nItter].ToLower () + "/";
+				if (temp.Contains ("plugins/ios/")) {
+					fileDirectory = dirs [nItter];
 					break;
 				}
 			}
+		} catch {
 		}
-		catch
-		{	}
 		return fileDirectory;
 	}
 }
--- Plugins/Crittercism_IOS_Scripts/CrittercismIOS.cs
+++ Plugins/Crittercism_IOS_Scripts/CrittercismIOS.cs
@@ -6,32 +6,32 @@ using System.Runtime.InteropServices;
 public static class CrittercismIOS
 {
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_EnableWithAppID (string appID);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_EnableWithAppID (string appID);
 
-    [DllImport("__Internal")]
-    private static extern bool Crittercism_LogHandledException (string name, string reason, string stack, int platformId);
+	[DllImport("__Internal")]
+	private static extern bool Crittercism_LogHandledException (string name, string reason, string stack, int platformId);
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_LogUnhandledException (string name, string reason, string stack, int platformId);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_LogUnhandledException (string name, string reason, string stack, int platformId);
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_SetAsyncBreadcrumbMode (bool writeAsync);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_SetAsyncBreadcrumbMode (bool writeAsync);
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_LeaveBreadcrumb (string breadcrumb);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_LeaveBreadcrumb (string breadcrumb);
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_SetUsername (string key);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_SetUsername (string key);
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_SetValue (string value, string key);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_SetValue (string value, string key);
 
-    [DllImport("__Internal")]
-    private static extern void Crittercism_SetOptOutStatus (bool status);
+	[DllImport("__Internal")]
+	private static extern void Crittercism_SetOptOutStatus (bool status);
 
-    [DllImport("__Internal")]
-    private static extern bool Crittercism_GetOptOutStatus ();
+	[DllImport("__Internal")]
+	private static extern bool Crittercism_GetOptOutStatus ();
 
 	[DllImport("__Internal")]
 	private static extern void Crittercism_BeginTransaction (string name);
@@ -56,7 +56,7 @@ public static class CrittercismIOS
 	private static extern int sigaction (Signal sig, IntPtr act, IntPtr oact);
 	
 	//SIGILL , SIGINT , SIGTERM
-	enum Signal 
+	enum Signal
 	{ 
 		SIGABRT = 6, 
 		SIGFPE = 8, 
@@ -68,74 +68,63 @@ public static class CrittercismIOS
 	// Crittercism-ios CRPluginException.h defines crPlatformId crUnityId = 0 .
 	private const int crUnityId = 0;
 
-    /// <summary>
-    /// Initializes Crittercism.  Crittercism must be initialized before any other calls may be
-    /// made to Crittercism.  Once Crittercism is initialized, any crashes will be reported to
-    /// Crittercism.
-    /// </summary>
-    /// <param name="appID">A Crittercism app identifier.  The app identifier may be found
-    /// in the Crittercism web portal under "App Settings".</param>
-    public static void Init (string appID)
-    {
-        if (Application.platform != RuntimePlatform.IPhonePlayer) {
-            Debug.Log ("CrittercismIOS only supports the iOS platform. Crittercism will not be enabled");
-            return;
-        }
-
-        if (appID == null) {
-            Debug.Log ("Crittercism given a null app ID");
-            return;
-        }
-
+	/// <summary>
+	/// Initializes Crittercism.  Crittercism must be initialized before any other calls may be
+	/// made to Crittercism.  Once Crittercism is initialized, any crashes will be reported to
+	/// Crittercism.
+	/// </summary>
+	/// <param name="appID">A Crittercism app identifier.  The app identifier may be found
+	/// in the Crittercism web portal under "App Settings".</param>
+	public static void Init (string appID)
+	{
+		if (Application.platform != RuntimePlatform.IPhonePlayer) {
+			Debug.Log ("CrittercismIOS only supports the iOS platform. Crittercism will not be enabled");
+			return;
+		}
+		if (appID == null) {
+			Debug.Log ("Crittercism given a null app ID");
+			return;
+		}
 		try {
+			// Signal handlers
 			int ptrSize;
-
-			// sizeof(struct sigaction) from <signal.h>
-			if(IntPtr.Size == 4) 
-			{
+			if (IntPtr.Size == 4) {
+				// sizeof(struct sigaction) from <signal.h>
 				ptrSize = 12; // 32-bit
-			}
-			// sizeof(struct sigaction) from <signal.h>
-			else 
-			{
+			} else {
+				// sizeof(struct sigaction) from <signal.h>
 				ptrSize = 16; // 64-bit
 			}
-
 			IntPtr sigabrt = Marshal.AllocHGlobal (ptrSize);
 			IntPtr sigfpe = Marshal.AllocHGlobal (ptrSize);
 			IntPtr sigbus = Marshal.AllocHGlobal (ptrSize);
 			IntPtr sigsegv = Marshal.AllocHGlobal (ptrSize);
-			
 			// Store Mono SIGSEGV and SIGBUS handlers
 			sigaction (Signal.SIGABRT, IntPtr.Zero, sigabrt);
 			sigaction (Signal.SIGFPE, IntPtr.Zero, sigfpe);
 			sigaction (Signal.SIGBUS, IntPtr.Zero, sigbus);
-			sigaction (Signal.SIGSEGV,IntPtr.Zero, sigsegv);
-			
+			sigaction (Signal.SIGSEGV, IntPtr.Zero, sigsegv);
 			Crittercism_EnableWithAppID (appID);
-			
 			// Restore or Destroy the handlers
 			sigaction (Signal.SIGABRT, sigabrt, IntPtr.Zero);  		//RESTORE
 			sigaction (Signal.SIGFPE, sigfpe, IntPtr.Zero);  		//RESTORE
 			sigaction (Signal.SIGBUS, sigbus, IntPtr.Zero);			//RESTORE
 			sigaction (Signal.SIGSEGV, sigsegv, IntPtr.Zero);		//RESTORE
-			
-			//Free sig structs
+			// Free sig structs
 			Marshal.FreeHGlobal (sigabrt);
 			Marshal.FreeHGlobal (sigfpe);
 			Marshal.FreeHGlobal (sigbus);
 			Marshal.FreeHGlobal (sigsegv);
+			// Add _OnUnresolvedExceptionHandler
+			System.AppDomain.CurrentDomain.UnhandledException += _OnUnresolvedExceptionHandler;
+			Application.RegisterLogCallback (_OnDebugLogCallbackHandler);
+			Debug.Log ("CrittercismIOS: Sucessfully Initialized");
+		} catch {
+			Debug.Log ("Crittercism Unity plugin failed to initialize.");
+		}
+	}
 
-            System.AppDomain.CurrentDomain.UnhandledException += _OnUnresolvedExceptionHandler;
-            Application.RegisterLogCallback (_OnDebugLogCallbackHandler);
-
-            Debug.Log ("CrittercismIOS: Sucessfully Initialized");
-        } catch {
-            Debug.Log ("Crittercism Unity plugin failed to initialize.");
-        }
-    }
-
-	private static string StackTrace(System.Exception e)
+	private static string StackTrace (System.Exception e)
 	{
 		// Allowing for the fact that the "name" and "reason" of the outermost
 		// exception e are already shown in the Crittercism portal, we don't
@@ -145,19 +134,19 @@ public static class CrittercismIOS
 		// outermost Exception e .
 		string answer = e.StackTrace;
 		// Using seen for cycle detection to break cycling.
-		List<System.Exception> seen = new List<System.Exception>();
-		seen.Add(e);
+		List<System.Exception> seen = new List<System.Exception> ();
+		seen.Add (e);
 		if (answer != null) {
 			// There has to be some way of telling where InnerException ie stacktrace
 			// ends and main Exception e stacktrace begins.  This is it.
-			answer = ((e.GetType().FullName + " : " + e.Message + "\r\n")
-			          + answer);
+			answer = ((e.GetType ().FullName + " : " + e.Message + "\r\n")
+				+ answer);
 			System.Exception ie = e.InnerException;
 			while ((ie != null) && (seen.IndexOf(ie) < 0)) {
-				seen.Add(ie);
-				answer = ((ie.GetType().FullName + " : " + ie.Message + "\r\n")
-				          + (ie.StackTrace + "\r\n")
-				          + answer);
+				seen.Add (ie);
+				answer = ((ie.GetType ().FullName + " : " + ie.Message + "\r\n")
+					+ (ie.StackTrace + "\r\n")
+					+ answer);
 				ie = ie.InnerException;
 			}
 		} else {
@@ -167,83 +156,83 @@ public static class CrittercismIOS
 	}
 
 	/// <summary>
-    /// Log an exception that has been handled in code.
-    /// This exception will be reported to the Crittercism portal.
-    /// </summary>
-    /// <param name="e">A caught exception that should be reported to Crittercism.</param>
-    static public void LogHandledException (System.Exception e)
-    {
-        if (e == null) {
-            return;
-        }
-        if (Application.platform == RuntimePlatform.IPhonePlayer) {
-			Crittercism_LogHandledException(e.GetType().FullName, e.Message, StackTrace(e), crUnityId);
-        }
-    }
+	/// Log an exception that has been handled in code.
+	/// This exception will be reported to the Crittercism portal.
+	/// </summary>
+	/// <param name="e">A caught exception that should be reported to Crittercism.</param>
+	static public void LogHandledException (System.Exception e)
+	{
+		if (e == null) {
+			return;
+		}
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Crittercism_LogHandledException (e.GetType ().FullName, e.Message, StackTrace (e), crUnityId);
+		}
+	}
 
-    /// <summary>
-    /// Check if the user has opted out of Crittercism.  If a user is opted out, then no data will be
-    /// sent to Crittercism.
-    /// </summary>
-    /// <returns>True if the user has opted out of Crittercism</returns>
-    static public bool GetOptOut ()
-    {
-        bool isOptedOut = true;
-        if (Application.platform == RuntimePlatform.IPhonePlayer) {
-            isOptedOut = Crittercism_GetOptOutStatus ();
-        }
-        return isOptedOut;
-    }
+	/// <summary>
+	/// Check if the user has opted out of Crittercism.  If a user is opted out, then no data will be
+	/// sent to Crittercism.
+	/// </summary>
+	/// <returns>True if the user has opted out of Crittercism</returns>
+	static public bool GetOptOut ()
+	{
+		bool isOptedOut = true;
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			isOptedOut = Crittercism_GetOptOutStatus ();
+		}
+		return isOptedOut;
+	}
 
-    /// <summary>
-    /// Changes whether the user is opted in or out of reporting data to Crittercism.
-    /// </summary>
-    /// <param name="isOptedOut">True to opt out of sending data to Crittercism</param>
-    static public void SetOptOut (bool isOptedOut)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer) {
-            Crittercism_SetOptOutStatus (isOptedOut);
-        }
-    }
+	/// <summary>
+	/// Changes whether the user is opted in or out of reporting data to Crittercism.
+	/// </summary>
+	/// <param name="isOptedOut">True to opt out of sending data to Crittercism</param>
+	static public void SetOptOut (bool isOptedOut)
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Crittercism_SetOptOutStatus (isOptedOut);
+		}
+	}
 
-    /// <summary>
-    /// Set the Username of the current user.
-    /// </summary>
-    /// <param name="username">The user name to set</param>
-    static public void SetUsername (string username)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer) {
-            Crittercism_SetUsername (username);
-        }
-    }
+	/// <summary>
+	/// Set the Username of the current user.
+	/// </summary>
+	/// <param name="username">The user name to set</param>
+	static public void SetUsername (string username)
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Crittercism_SetUsername (username);
+		}
+	}
 
-    /// <summary>
-    /// Tell Crittercism to associate the given value/key pair with the current
-    /// device UUID.
-    /// <param name="val">The metadata value to set</param>
-    /// <param name="key">The key to associate with the given metadata<c/param>
-    /// <example>SetValue("5", "Game Level")</example>
-    /// </summary>
-    static public void SetValue (string val, string key)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer) {
-            Crittercism_SetValue (val, key);
-        }
-    }
+	/// <summary>
+	/// Tell Crittercism to associate the given value/key pair with the current
+	/// device UUID.
+	/// <param name="val">The metadata value to set</param>
+	/// <param name="key">The key to associate with the given metadata<c/param>
+	/// <example>SetValue("5", "Game Level")</example>
+	/// </summary>
+	static public void SetValue (string val, string key)
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Crittercism_SetValue (val, key);
+		}
+	}
 
-    /// <summary>
-    /// Log a breadcrumb.  Breadcrumbs are used for tracking local events.  Breadcrumbs
-    /// will be attached to handled exceptions and crashes, which will allow diagnosing
-    /// which events lead up to a crash.
-    /// </summary>
-    /// <param name="breadcrumb">The breadcrumb text to append to the breadcrumb trail</param>
-    /// <example>LeaveBreadcrumb("Game started");</example>
-    static public void LeaveBreadcrumb (string breadcrumb)
-    {
-        if (Application.platform == RuntimePlatform.IPhonePlayer) {
-            Crittercism_LeaveBreadcrumb (breadcrumb);
-        }
-    }
+	/// <summary>
+	/// Log a breadcrumb.  Breadcrumbs are used for tracking local events.  Breadcrumbs
+	/// will be attached to handled exceptions and crashes, which will allow diagnosing
+	/// which events lead up to a crash.
+	/// </summary>
+	/// <param name="breadcrumb">The breadcrumb text to append to the breadcrumb trail</param>
+	/// <example>LeaveBreadcrumb("Game started");</example>
+	static public void LeaveBreadcrumb (string breadcrumb)
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Crittercism_LeaveBreadcrumb (breadcrumb);
+		}
+	}
 
 	/// <summary>
 	/// Begin a transaction to track ex. login
@@ -289,7 +278,8 @@ public static class CrittercismIOS
 	{
 		if (Application.platform == RuntimePlatform.IPhonePlayer) {
 			Crittercism_SetTransactionValue (name, value);
-		};
+		}
+		;
 	}
 	
 	/// <summary>
@@ -304,34 +294,34 @@ public static class CrittercismIOS
 		}
 	}
 
-    static private void _OnUnresolvedExceptionHandler (object sender, System.UnhandledExceptionEventArgs args)
-    {
-        if (args == null || args.ExceptionObject == null) {
-            return;
-        }
-        try {
-            System.Type type = args.ExceptionObject.GetType ();
-            if (type == typeof(System.Exception)) {
-                System.Exception e = (System.Exception)args.ExceptionObject;
+	static private void _OnUnresolvedExceptionHandler (object sender, System.UnhandledExceptionEventArgs args)
+	{
+		if (args == null || args.ExceptionObject == null) {
+			return;
+		}
+		try {
+			System.Type type = args.ExceptionObject.GetType ();
+			if (type == typeof(System.Exception)) {
+				System.Exception e = (System.Exception)args.ExceptionObject;
 				if (Application.platform == RuntimePlatform.IPhonePlayer) {
-                    // Should never get here since the Init() call would have bailed on the same if statement
-					Crittercism_LogUnhandledException(e.GetType().FullName, e.Message, StackTrace(e), crUnityId);
-                }
-            }
-        } catch {
-            if (Debug.isDebugBuild == true) {
-                Debug.Log ("CrittercismIOS: Failed to log exception");
-            }
-        }
-    }
+					// Should never get here since the Init() call would have bailed on the same if statement
+					Crittercism_LogUnhandledException (e.GetType ().FullName, e.Message, StackTrace (e), crUnityId);
+				}
+			}
+		} catch {
+			if (Debug.isDebugBuild == true) {
+				Debug.Log ("CrittercismIOS: Failed to log exception");
+			}
+		}
+	}
 
-    static private void _OnDebugLogCallbackHandler (string name, string stack, LogType type)
-    {
-        if (LogType.Exception == type || LogType.Assert == type) {
-            if (Application.platform == RuntimePlatform.IPhonePlayer) {
-                // Should never get here since the Init() call would have bailed on the same if statement
+	static private void _OnDebugLogCallbackHandler (string name, string stack, LogType type)
+	{
+		if (LogType.Exception == type || LogType.Assert == type) {
+			if (Application.platform == RuntimePlatform.IPhonePlayer) {
+				// Should never get here since the Init() call would have bailed on the same if statement
 				Crittercism_LogUnhandledException (name, name, stack, crUnityId);
-            }
-        }
-    }
+			}
+		}
+	}
 }
